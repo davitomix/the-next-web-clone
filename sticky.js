3171b53706b3fdf689af442d537e2b1930f238dc
davitomix <davidelimg1mail.com>
2019-09-09 10:14:43 -0500

    layout 1

--- sticky.js
+++ sticky.js
@@ -7,10 +7,11 @@ var navbar = document.getElementById("sticky-nav");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
